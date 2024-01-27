0e5847562c86020b723392267c8d8770afd5092b
kyoh86 <me@kyoh86.dev>
2024-01-28 08:39:34 +0900

    Update dependencies

--- command.ts
+++ command.ts
@@ -1,5 +1,5 @@
 import type { Denops } from "https://deno.land/x/denops_std@v5.3.0/mod.ts";
-import { TextLineStream } from "https://deno.land/std@0.212.0/streams/text_line_stream.ts";
+import { TextLineStream } from "https://deno.land/std@0.213.0/streams/text_line_stream.ts";
 import { EchomsgStream } from "./echomsg_stream.ts";
 
 /**
