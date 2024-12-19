65ec847ec662b3278d77eeec3e18d3e559f062dc
kyoh86 <me@kyoh86.dev>
2024-12-19 16:41:52 +0900

    add "map" module

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # denops-util
 
-It's a set of denops utils just for me. I will make destructive changes without
-any notice.
+It's a set of denops utils just for me. I will make destructive changes without any notice.
---
+++ map.ts
@@ -0,0 +1,38 @@
+import * as fn from "jsr:@denops/std@~7.4.0/function";
+import * as mapping from "jsr:@denops/std@~7.4.0/mapping";
+import type { MapOptions } from "jsr:@denops/std@7/mapping";
+import type { Denops } from "jsr:@denops/std@~7.4.0";
+
+/**
+ * Make a mapping to dispatch a denops method call.
+ */
+export async function dispatch(
+  args: {
+    denops: Denops;
+    lhs: string;
+    method: string;
+    sync?: boolean;
+    name?: string;
+    args?: unknown[];
+  } & MapOptions,
+): Promise<void> {
+  const { denops, lhs, method, ...opts } = {
+    sync: false,
+    name: args.denops.name,
+    args: [],
+    mode: "n",
+    ...args,
+  };
+  return await mapping.map(
+    denops,
+    lhs,
+    `<cmd>call denops#${opts.sync ? "request" : "notify"}(${await fn.string(
+      denops,
+      opts.name,
+    )}, ${await fn.string(denops, method)}, ${await fn.string(
+      denops,
+      opts.args,
+    )})<cr>`,
+  );
+}
+
